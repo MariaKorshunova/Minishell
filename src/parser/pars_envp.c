f22ae6b75d25af695f9366ace934b2e294c125c0
Jone Mabel <jmabel@at-d5.msk.21-school.ru>
2022-07-17 21:05:17 +0300

    Start lexer, need change Makefile

--- src/parser/pars_envp.c
+++ src/parser/pars_envp.c
@@ -6,7 +6,7 @@
 /*   By: jmabel <jmabel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 15:24:23 by jmabel            #+#    #+#             */
-/*   Updated: 2022/07/15 11:17:57 by jmabel           ###   ########.fr       */
+/*   Updated: 2022/07/17 20:56:10 by jmabel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,21 @@
 void	pars_envp(t_env *list_env, char **envp)
 {
 	int		i;
-	int		j;
-	char	*name;
+	// int		j;
+	// char	*name;
 
 	(void)list_env;
+	(void)envp;
 	i = 0;
-	while (envp[i])
-	{
-		j = 0;
-		while (envp[i][j] != '=')
-			j++;
-		// handle error with '=' в envp (j=0 or j=len(str))
-		name = ft_substr(envp[i], 0, j);
-		printf("%d\t%s\t%s\n", j, name, envp[i++]);
-	}
+	// while (envp[i])
+	// {
+	// 	j = 0;
+	// 	while (envp[i][j] != '=')
+	// 		j++;
+	// 	// handle error with '=' в envp (j=0 or j=len(str))
+	// 	name = ft_substr(envp[i], 0, j);
+	// 	printf("%d\t%s\t%s\n", j, name, envp[i++]);
+	// }
 }
 
 // static void	lstnew_env(char *name, char *content);
