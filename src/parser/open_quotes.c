1a07b3f3fa76071a891a62dbdde40592590f07d3
Jone Mabel <jmabel@at-j6.msk.21-school.ru>
2022-08-31 14:03:59 +0300

    start open_qoutes

---
+++ src/parser/open_quotes.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   open_quotes.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmabel <jmabel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 13:38:29 by jmabel            #+#    #+#             */
+/*   Updated: 2022/08/31 13:43:57 by jmabel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+/* glue consecutive tokens WORD, QUOTE, DOUBLE_QUOTE  with opening quotes.
+ Pointer to token_list is moved.
+ Pointer can't be NULL */
+
+int	open_quotes(t_key_val *token)
+{
+	if (token == NULL)
+		return (EXIT_SUCCESS);
+	
+}
