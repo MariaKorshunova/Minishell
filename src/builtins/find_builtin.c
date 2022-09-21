317a87b75db5d2fe43208f62ed6814e05d76231e
Reek Efrain <refrain@at-l4.msk.21-school.ru>
2022-09-21 20:09:14 +0300

    find_builtin edited; txt files deleted

--- src/builtins/find_builtin.c
+++ src/builtins/find_builtin.c
@@ -6,14 +6,42 @@
 /*   By: refrain <refrain@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 23:01:13 by refrain           #+#    #+#             */
-/*   Updated: 2022/09/21 18:44:18 by refrain          ###   ########.fr       */
+/*   Updated: 2022/09/21 20:07:23 by refrain          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
+static int	ft_add_function(char **cmd, t_data *data)
+{
+	if (ft_strcmp(cmd[0], "exit") == 0)
+	{
+		ft_exit(cmd, data);
+		return (1);
+	}
+	else if (ft_strcmp(cmd[0], "env") == 0 || ft_strcmp(cmd[0], "ENV") == 0)
+	{
+		ft_env(data);
+		return (1);
+	}
+	else if (ft_strcmp(cmd[0], "export") == 0)
+	{
+		if (ft_export(cmd, data) == -1)
+			return (-1);
+		return (1);
+	}
+	else if (ft_strcmp(cmd[0], "unset") == 0)
+	{
+		ft_unset(cmd, data);
+		return (1);
+	}
+	return (0);
+}
+
 int	find_builtin(char **cmd, t_data *data)
 {
+	int	status;
+
 	if (ft_strcmp(cmd[0], "echo") == 0 || ft_strcmp(cmd[0], "ECHO") == 0)
 	{
 		ft_echo(cmd);
@@ -30,26 +58,10 @@ int	find_builtin(char **cmd, t_data *data)
 			return (-1);
 		return (1);
 	}
-	else if (ft_strcmp(cmd[0], "env") == 0 || ft_strcmp(cmd[0], "ENV") == 0)
+	else
 	{
-		ft_env(data);
-		return (1);
-	}
-	if (ft_strcmp(cmd[0], "exit") == 0)
-	{
-		ft_exit(cmd, data);
-		return (1);
-	}
-	else if (ft_strcmp(cmd[0], "export") == 0)
-	{
-		if (ft_export(cmd, data) == -1)
-			return (-1);
-		return (1);
-	}
-	else if (ft_strcmp(cmd[0], "unset") == 0)
-	{
-		ft_unset(cmd, data);
-		return (1);
+		status = ft_add_function(cmd, data);
+		return (status);
 	}
 	return (0);
 }
